98515f660dcc395b6c844038082a7194df8b46b2
Jonan Seeley <jonans99@yahoo.com>
2016-12-05 12:49:50 -0500

    test

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
-
+<img src="uploads/test2.jpg" height="42" width="42">
 </body>
 </html>
